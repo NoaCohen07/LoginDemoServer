3bf553c365080d95d0351d27fac9c929c3eb7a77
User <User@LAB2-7>
2024-05-06 14:08:20 +0300

    vhh

--- LoginDemoServer/DBScripts/Create.sql
+++ LoginDemoServer/DBScripts/Create.sql
@@ -12,7 +12,20 @@ Create Table Users (
 )
 Go
 
+Create Table Grades(
+DateofTest DateTime NULL,
+subjectName nvarchar (20) NULL,
+Grade int NOT NULL,
+Email nvarchar(100) FOREIGN KEY REFERENCES Users(Email)
+)
+GO
+
+
 INSERT INTO dbo.Users VALUES ('ofer@ofer.com', '1234', '+972526344450','15-nov-1972',1,'Ofer Zadikario')
 Go
 
---scaffold-DbContext "Server = (localdb)\MSSQLLocalDB;Initial Catalog=LoginDemoDB;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Encrypt=False;Trust Server Certificate=False;Command Timeout=0" Microsoft.EntityFrameworkCore.SqlServer -OutPutDir Models -Context LoginDemoDbContext -DataAnnotations -force
+Insert INTO dbo.Grades VALUES('2014-4-5', 'Math', 99, 'ofer@ofer.com')
+Go
+
+SELECT * FROM Grades
+--scaffold-DbContext "Server = (localdb)\LAB2-7\SQLEXPRESS;Initial Catalog=LoginDemoDB;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Encrypt=False;Trust Server Certificate=False;Command Timeout=0" Microsoft.EntityFrameworkCore.SqlServer -OutPutDir Models -Context LoginDemoDbContext -DataAnnotations -force
